87fe506a8757d8f7f962b9267243fc3cd0395383
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-01-22 16:50:30 -0500

    Create ThemeChangeView file

---
+++ Theme Switcher/Helpers/ThemeChangeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeChangeView.swift
+//  Theme Switcher
+//
+//  Created by Leonard McCook-Carr on 1/22/24.
+//
+
+import SwiftUI
+
+struct ThemeChangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeChangeView()
+}
