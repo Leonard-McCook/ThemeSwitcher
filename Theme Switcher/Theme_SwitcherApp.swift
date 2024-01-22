7631126c182237229ba42a499dba8980c6f10dfb
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-01-21 20:52:28 -0500

    Initial Commit

---
+++ Theme Switcher/Theme_SwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Theme_SwitcherApp.swift
+//  Theme Switcher
+//
+//  Created by Leonard McCook-Carr on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Theme_SwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
