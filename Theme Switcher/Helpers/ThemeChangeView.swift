3f7b612b9ec1031d70675e2e368e4a936b5c5fc0
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-01-24 12:25:21 -0500

    Create Theme enum

--- Theme Switcher/Helpers/ThemeChangeView.swift
+++ Theme Switcher/Helpers/ThemeChangeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ThemeChangeView: View {
     @Environment(\.colorScheme) private var scheme
+    @AppStorage("user_theme") private var userTheme: Theme = .systemDefault
     var body: some View {
         VStack(spacing: 15) {
             
@@ -18,4 +19,23 @@ struct ThemeChangeView: View {
 
 #Preview {
     ThemeChangeView()
+}
+
+
+/// Theme
+enum Theme: String, CaseIterable {
+    case systemDefault = "Default"
+    case light = "Light"
+    case dark = "Dark"
+    
+    func color(_ scheme: ColorScheme) -> Color {
+        switch self {
+        case .systemDefault:
+            return scheme == .dark ? .moon : .sun
+        case .light:
+            return .sun
+        case .dark:
+            return .moon
+        }
+    }
 }
